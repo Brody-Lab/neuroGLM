566e82e983929a531499975112ca1fe14da688ca
Adrian Bondy <adrian.bondy@gmail.com>
2019-10-11 17:20:28 -0400

    remove assumption that data is in a mountainsort folder

--- fit_glm_to_Cells_batch.m
+++ fit_glm_to_Cells_batch.m
@@ -1,19 +1,19 @@
-function res = fit_glm_to_Msorted_batch(rat,varargin)
+function res = fit_glm_to_Cells_batch(rat,varargin)
     p=inputParser;
     p.KeepUnmatched=true;
     p.addParameter('remake',true,@(x)validateattributes(x,{'logical'},{'scalar'}));
-    p.addParameter('mountainsort_fldr','X:\RATTER\PhysData\Mountainsort\Adrian',@(x)isdir(x));
+    p.addParameter('sort_fldr','X:\RATTER\PhysData\Mountainsort\Adrian',@(x)isdir(x));
     p.parse(varargin{:});
     params=p.Results;
-    if ~isdir([params.mountainsort_fldr,filesep,rat])
-        error('\nNo data in %s for rat %s.',params.mountainsort_fldr,rat);
+    if ~isdir([params.sort_fldr,filesep,rat])
+        error('\nNo data in %s for rat %s.',params.sort_fldr,rat);
     end
-    possible_sessions = dir([params.mountainsort_fldr,filesep,rat]);
+    possible_sessions = dir([params.sort_fldr,filesep,rat]);
     possible_sessions = {possible_sessions.name};
     possible_sessions = possible_sessions(strncmp(possible_sessions,'20',2));
     for i=1:length(possible_sessions)
         fprintf('\nWorking on rat %s, folder %s.\n',rat,possible_sessions{i});
-        msorted_filename = fixfilesep([params.mountainsort_fldr,filesep,rat,filesep,possible_sessions{i},filesep,'Msorted_',rat,'_',possible_sessions{i},'.mat']); 
+        msorted_filename = fixfilesep([params.sort_fldr,filesep,rat,filesep,possible_sessions{i},filesep,'Msorted_',rat,'_',possible_sessions{i},'.mat']); 
         if ~exist(msorted_filename,'file')
             res{i} = 'no msorted';
             fprintf([res{i},'\n']);
@@ -21,9 +21,25 @@ function res = fit_glm_to_Msorted_batch(rat,varargin)
         end
         fprintf('Loading %s.\n',msorted_filename);
         file_contents = load(msorted_filename);
+        if ~any(file_contents.Msorted.n_cluster) && ~any(file_contents.Cells.n_cluster)
+            res{i} = 'no cells';
+            fprintf([res{i},'\n']);
+            continue
+        end
         try
-            fit_glm_to_Msorted(file_contents.Msorted,'use_parallel',true,'save',true,varargin{:});
-            res{i} = 'success';
+            try
+                file_contents.Msorted.Trials = add_shuffled_click_times(file_contents.Msorted.Trials);
+                file_contents.Msorted = PB_make_PETH(file_contents.Msorted);
+                [a,b,c] = fileparts(msorted_filename);
+                fit_glm_to_Cells(file_contents.Msorted,'use_parallel',true,'save',true,varargin{:},'save_path',[a,filesep,b,'_glmfits.mat']);
+                res{i} = 'success';
+            catch
+                file_contents.Cells.Trials = add_shuffled_click_times(file_contents.Cells.Trials);
+                file_contents.Cells = PB_make_PETH(file_contents.Cells);
+                [a,b,c] = fileparts(msorted_filename);
+                fit_glm_to_Cells(file_contents.Cells,'use_parallel',true,'save',true,varargin{:},'save_path',[a,filesep,b,'_glmfits.mat']);
+                res{i} = 'success';                
+            end
         catch ME
             res{i} = ME;
             fprintf('Encountered error in glm fitting on rat %s, folder %s.\n',rat,possible_sessions{i});
