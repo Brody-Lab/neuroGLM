3e22e6624e7a02f0e8caeb457e9666e17cbdca1c
Adrian Bondy <adrian.bondy@gmail.com>
2020-07-01 13:05:43 -0400

    fixed crash due to experiment id looking for missing fields in Cells files

--- PBups/build_expt_for_pbups.m
+++ PBups/build_expt_for_pbups.m
@@ -3,14 +3,21 @@ function expt = build_expt_for_pbups(rawData,varargin)
     p.addParameter('bin_size_s',0.001,@(x)validateattributes(x,{'numeric'},{'scalar','positive'}));
     p.parse(varargin{:});
     params=p.Results;
-    expt = buildGLM.initExperiment(rawData.param.samplingFreq*params.bin_size_s, [rawData.param.rat,', ',rawData.param.sess_date],rawData.param);
+    if isfield(rawData.param,'sess_date')   
+        id= [char(rawData.param.rat),', ',char(rawData.param.sess_date)];        
+    elseif isfield(rawData.param,'sessid')
+        id=rawData.param.sessid;
+    elseif isfield(rawData.param,'mat_file_name')
+        id=rawData.param.mat_file_name;
+    end 
+    expt = buildGLM.initExperiment(rawData.param.samplingFreq*params.bin_size_s, id,rawData.param);
     for f=1:length(rawData.timings)
         if isfield(rawData.trial,rawData.timings{f})
             expt = buildGLM.registerTiming(expt,rawData.timings{f},rawData.timings{f});
         end
     end
     for c=1:rawData.param.ncells
-        expt = buildGLM.registerSpikeTrain(expt,['sptrain',num2str(c)],[rawData.param.rat,', ',rawData.param.sess_date,', cell ',num2str(c)]);
+        expt = buildGLM.registerSpikeTrain(expt,['sptrain',num2str(c)],[num2str(id),', cell ',num2str(c)]);
     end
     expt = buildGLM.registerValue(expt,'pokedR','chose right');
     expt = buildGLM.registerValue(expt,'ishit','correct trial');
